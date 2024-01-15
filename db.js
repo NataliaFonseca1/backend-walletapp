31e564e094c45b71f40de68863a93679adf2e7ee
NataliaFonseca1 <fonsecanatalianf@gmail.com>
2024-01-15 16:36:04 -0300

    first commit

---
+++ db.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+const db = new Pool({
+    user: 'docker',
+    host: 'localhost',
+    database: 'finances',
+    password: 'docker',
+    port: 5432,
+    
+
+})
+
+module.exports = db;
